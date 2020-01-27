74939bd48f7bac7a587b357364e22249ad03e7a3
unknown <Kat2bk@gmail.com>
2020-01-27 13:51:50 -0800

    added imports and Routes to app.js. was able to display movieList, working on functionality

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -8,6 +9,7 @@ const MovieList = props => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
+          console.log("axios call", response)
           setMovies(response.data);
         })
         .catch(error => {
@@ -31,6 +33,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
+      <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,6 +48,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      </Link>
     </div>
   );
 }
